8da692cb4d711babbccb6110fbf0387ba67394bf
David Audrain <daudrain@net-sense.fr>
2016-02-14 15:36:10 +0100

    Persist full list of segments

--- api/segments.py
+++ api/segments.py
@@ -1,12 +1,45 @@
+import argparse
 import constants
 import json
+import os
 from helper import http
-class Segments:
+
+SEGMENTS_API_NAME="segments"
+
+class Rest:
+
+    CMD_LIST = 'list'
+    CMD_LIST_STORE = 'liststore'
+    
+    COMMANDS = [CMD_LIST, CMD_LIST_STORE]
 
     api_base = constants.TOP_LEVEL_URL + "/service/rest/segments/"
 
     def __init__(self, appid):
         self.appid = appid
 
-    def fetch_list(self):
-        return http.get_json(self.api_base, "list", {'config':json.dumps({'sortField': 'name', 'sortDirection': 'asc', 'limit': 3})})
+    def process(self, input_args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--cmd', dest='cmd', help='Command', default=Rest.CMD_LIST, choices=Rest.COMMANDS)
+        parser.add_argument('--limit', dest='limit', help='Number of segments', default=3, type=int)
+        parser.add_argument('--path', dest='path', help='Path where to read and write data', default='data')
+
+        args = parser.parse_args(input_args)
+
+        if args.cmd == Rest.CMD_LIST:
+            response = http.get_json(self.api_base, "list", {'config':json.dumps({'sortField': 'name', 'sortDirection': 'asc', 'limit': args.limit})})
+            print json.dumps(response, sort_keys=True, indent=2)
+        elif args.cmd == Rest.CMD_LIST_STORE:
+            if args.path != None:
+                app_path = os.path.join(args.path, self.appid, 'segments')
+                if not os.path.isdir(app_path):
+                    os.makedirs(app_path)
+                response = http.get_json(self.api_base, "list", {'config':json.dumps({'sortField': 'name', 'sortDirection': 'asc', 'limit': args.limit})})
+                response = http.get_json(self.api_base, "list", {'config':json.dumps({'sortField': 'name', 'sortDirection': 'asc', 'limit': response["totalLength"]})})
+                list_json = open(os.path.join(app_path, "list.json"), 'w')
+                list_json.write(json.dumps(response, sort_keys=True, indent=2))
+                list_json.close()
+            else:
+                print 'missing path argument'
+        else:
+            print "unknown command " + args.cmd
--- capptain.py
+++ capptain.py
@@ -7,16 +7,19 @@ from helper import http
 from api import constants 
 from api import segments
 
-parser = argparse.ArgumentParser(prog='PROG')
-parser.add_argument('--appid', dest='appid', help='Application ID')
-parser.add_argument('--apikey', dest='apikey', help='API Key')
+parser = argparse.ArgumentParser()
+parser.add_argument('--appid', required=True, dest='appid', help='Application ID')
+parser.add_argument('--apikey', required=True, dest='apikey', help='API Key')
+parser.add_argument('--api', required=True, dest='api', help='API', choices=[segments.SEGMENTS_API_NAME])
 
-args = parser.parse_args()
+# Parse known arguments at this point
+# other arguments may be known of underlying API
+(known_args, unknown_args) = parser.parse_known_args()
 
-print constants.TOP_LEVEL_URL
+http.setbasicauthentication(top_level_url=constants.TOP_LEVEL_URL, appid=known_args.appid, apikey=known_args.apikey)
 
-http.setbasicauthentication(top_level_url=constants.TOP_LEVEL_URL, appid=args.appid,apikey=args.apikey)
-
-segments_api = segments.Segments(args.appid)
-
-print json.dumps(segments_api.fetch_list(), sort_keys=True, indent=2)
+if known_args.api == segments.SEGMENTS_API_NAME:
+    segments_api = segments.Rest(known_args.appid)
+    segments_api.process(unknown_args)
+else:
+    print "unknown api" 
