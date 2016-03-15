8a51fccf0689673e51e811352713096e8dada943
David Audrain <daudrain@net-sense.fr>
2016-03-15 14:53:14 +0100

    Add support for application registry. One can now execute commands on multiple Capptain applications.

--- README.md
+++ README.md
@@ -52,3 +52,40 @@ Here is a sample json
   "name": "MySegmentName"
 }
 ```
+
+### Operate on many applications at a time
+
+One need to list the applications in a JSON file like
+```
+{
+  "apps": [
+    {
+      "name": "app_a", 
+      "appid": "<app_a id>", 
+      "apikey": "<app_a apikey>",
+      "monitorapikey": "<app_a monitoringapikey>"
+    },
+    {
+      "name": "app_b", 
+      "appid": "<app_b id>", 
+      "apikey": "<app_b apikey>",
+      "monitorapikey": "<app_b monitoringapikey>"
+    },
+    {
+      "name": "app_c", 
+      "appid": "<app_c id>", 
+      "apikey": "<app_c apikey>",
+      "monitorapikey": "<app_c monitoringapikey>"
+    }
+  ]
+}
+```
+
+Finally appid and apikey arguments are replaced by the appregistry argument.
+appregistry references the application registry json file.
+
+`python capptain.py --appregistry myappregistry.json --api segments --limit 5`
+
+A subset of the registry can be addressed by specifying the apps argument:
+
+`python capptain.py --appregistry --apps "app_a;app_c" myappregistry.json --api segments --limit 5`
--- capptain.py
+++ capptain.py
@@ -7,19 +7,42 @@ from helper import http
 from api import constants 
 from api import segments
 
+
+def application_process_arguments(appid, apikey, arguments):
+    http.setbasicauthentication(top_level_url=constants.TOP_LEVEL_URL, appid=appid, apikey=apikey)
+
+    if known_args.api == segments.SEGMENTS_API_NAME:
+        segments_api = segments.Rest(appid)
+        segments_api.process(unknown_args)
+    else:
+        print "unknown api" 
+    
+
 parser = argparse.ArgumentParser()
-parser.add_argument('--appid', required=True, dest='appid', help='Application ID')
-parser.add_argument('--apikey', required=True, dest='apikey', help='API Key')
+parser.add_argument('--appid', dest='appid', help='Application ID. See Capptain application settings page.')
+parser.add_argument('--apikey', dest='apikey', help='API Key')
+parser.add_argument('--appregistry', dest='appregistry', help='JSON file listing appid and apikey by appname')
+parser.add_argument('--apps', dest='apps', help='List of app names to apply the command')
 parser.add_argument('--api', required=True, dest='api', help='API', choices=[segments.SEGMENTS_API_NAME])
 
 # Parse known arguments at this point
 # other arguments may be known of underlying API
 (known_args, unknown_args) = parser.parse_known_args()
 
-http.setbasicauthentication(top_level_url=constants.TOP_LEVEL_URL, appid=known_args.appid, apikey=known_args.apikey)
-
-if known_args.api == segments.SEGMENTS_API_NAME:
-    segments_api = segments.Rest(known_args.appid)
-    segments_api.process(unknown_args)
+if (known_args.appid != None and known_args.apikey != None):
+    application_process_arguments(known_args.appid, known_args.apikey, unknown_args)
+elif (known_args.appregistry != None):
+    list_json = open(known_args.appregistry, 'r')
+    json_content = json.load(list_json)
+    list_json.close()
+    list_of_apps = json_content["apps"]
+    if (known_args.apps != None):
+        apps = known_args.apps.split(";")
+        filtered_apps = filter(lambda app: app["name"] in apps, list_of_apps)
+    else:
+        filtered_apps = list_of_apps
+    for app in filtered_apps:
+        print "Process app " + app["name"]
+        application_process_arguments(app["appid"], app["apikey"], unknown_args)
 else:
-    print "unknown api" 
+    parser.print_help() 
